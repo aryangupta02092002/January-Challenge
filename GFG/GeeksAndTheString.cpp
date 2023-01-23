e5e68e6f5d2ba7bd27a2047356a56e085e595b40
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-01-23 21:22:35 +0530

    Jan 23

---
+++ GFG/GeeksAndTheString.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removePair(string s) {
+        // code here
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            if(!st.empty() && st.top() == s[i]){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        string ans = "";
+        stack<char> st1;
+        while(!st.empty()){
+            st1.push(st.top());
+            st.pop();
+        }
+        
+        while(!st1.empty()){
+            ans += st1.top();
+            st1.pop();
+        }
+        return ans == "" ? "-1" : ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s; 
+        cin>>s;
+        
+        Solution obj;
+        string res = obj.removePair(s);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
