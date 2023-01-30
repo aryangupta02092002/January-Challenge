ae396b394644f03ddd57cc8ea81b7d2d600dcc26
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-01-30 21:43:52 +0530

    Jan 30

---
+++ Leetcode/1137_NthTribonacciNumbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n+1, INT_MAX);
+      if(n < 2)
+          return n;
+      if(n==2)
+          return n-1;
+      dp[0] = 0;
+      dp[1] = 1;
+      dp[2] = 1;
+      
+      for(int i=3; i<dp.size(); i++){
+        dp[i] = min(dp[i], dp[i-1] + dp[i-2] + dp[i-3]);
+      }
+      return dp[n];
+    }
+};
